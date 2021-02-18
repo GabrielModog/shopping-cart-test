632987599be1d152728f212808c5d4efa36254ab
GabrielModog <gabrielmodog@gmail.com>
2021-02-18 00:06:08 -0300

    adds subtotal unit test

---
+++ src/tests/getSubtotal.test.ts
@@ -0,0 +1,26 @@
+import React from 'react';
+
+describe('Get subtotal test suite', () => {
+  test('Should return subtotal prices from list: [20, 50, 20, 20]', () => {
+    expect(
+      getSubtotal([
+        {
+          price: 20,
+          available: 1,
+        },
+        {
+          price: 50,
+          available: 1,
+        },
+        {
+          price: 20,
+          available: 1,
+        },
+        {
+          price: 20,
+          available: 1,
+        },
+      ])
+    ).toBe(110);
+  });
+});
